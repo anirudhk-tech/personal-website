ba46c1050562bcfc291c130fcf015ea796572e19
unknown <anirudhkuppili@gmail.com>
2025-02-28 16:21:11 -0600

    Model rendering fixed2.

--- src/app/components/project/model/lazyLoadModel.tsx
+++ src/app/components/project/model/lazyLoadModel.tsx
@@ -17,7 +17,7 @@ export const LazyLoadModel = ({ children }: { children: React.ReactNode }) => {
           }
         });
       },
-      { threshold: 0.0, rootMargin: "0px 0px -500px 0px" }
+      { threshold: 0.0 }
     );
 
     if (containerRef.current) {
