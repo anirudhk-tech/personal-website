54b6731c0dae1ffac11ed55c1c78ead60b9eb1e3
unknown <anirudhkuppili@gmail.com>
2025-03-05 09:13:13 -0600

    Added google analytics 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "@fontsource/montserrat";
 import "@fontsource/orbitron";
 import { ThemeWrapper } from "@/themeWrapper";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Kuppili",
@@ -18,6 +19,7 @@ export default function RootLayout({
     <ThemeWrapper>
       <html lang="en">
         <body>{children}</body>
+        <GoogleAnalytics gaId="G-GW2X2VVGD6" />
       </html>
     </ThemeWrapper>
   );
