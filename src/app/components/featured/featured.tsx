ef41ce343b87d8ef282f0c5ca9d5629893c6f030
Anirudh Kuppili <anirudhkuppili@gmail.com>
2025-09-10 11:29:36 -0500

    changed featured

--- src/app/components/featured/featured.tsx
+++ src/app/components/featured/featured.tsx
@@ -6,7 +6,7 @@ import { Typography } from "@mui/material";
 import { FadeInOnScroll } from "@/app/animations/fadeinOnScroll";
 import { Container, GridContainer, Square, Caption } from "./featured.styles";
 
-import Shiplo from "@/../public/featured/shiplo.png";
+import Instagram from "@/../public/featured/instagram.jpeg";
 import X from "@/../public/featured/x.png";
 import Medium from "@/../public/featured/medium.png";
 
@@ -18,45 +18,44 @@ type FeaturedItem = {
 
 const featured: FeaturedItem[] = [
   {
-    src: Shiplo.src,
-    caption: "Shiplo - Autonomous operators",
-    href: "https://tryshiplo.com/",
+    src: Instagram.src,
+    caption: "Instagram - Tested insights",
+    href: "https://www.instagram.com/venkata.ani/",
   },
   {
     src: X.src,
-    caption: "X (Twitter) - Live Insights.",
+    caption: "X (Twitter) - Random thoughts",
     href: "https://x.com/venkata_ani",
   },
   {
     src: Medium.src,
-    caption: "Articles - Building in Words",
+    caption: "Articles - Explaining ideas",
     href: "https://medium.com/@anirudhkuppili",
   },
 ];
 
 export const Featured = () => (
-  <></>
-  // <Container>
-  //   <FadeInOnScroll>
-  //     <Typography variant="h2">Featured</Typography>
-  //   </FadeInOnScroll>
+  <Container>
+    <FadeInOnScroll>
+      <Typography variant="h2">Where I Am At</Typography>
+    </FadeInOnScroll>
 
-  //   <GridContainer>
-  //     {featured.map(({ src, caption, href }) => (
-  //       <FadeInOnScroll key={caption}>
-  //         <Link href={href} passHref legacyBehavior target="_blank">
-  //           <Square>
-  //             <Image
-  //               src={src}
-  //               alt={caption}
-  //               fill
-  //               style={{ objectFit: "cover" }}
-  //             />
-  //             <Caption variant="body1">{caption}</Caption>
-  //           </Square>
-  //         </Link>
-  //       </FadeInOnScroll>
-  //     ))}
-  //   </GridContainer>
-  // </Container>
+    <GridContainer>
+      {featured.map(({ src, caption, href }) => (
+        <FadeInOnScroll key={caption}>
+          <Link href={href} passHref legacyBehavior target="_blank">
+            <Square>
+              <Image
+                src={src}
+                alt={caption}
+                fill
+                style={{ objectFit: "cover" }}
+              />
+              <Caption variant="body1">{caption}</Caption>
+            </Square>
+          </Link>
+        </FadeInOnScroll>
+      ))}
+    </GridContainer>
+  </Container>
 );
