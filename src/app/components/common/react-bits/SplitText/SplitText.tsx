03c45102365aa006f648c665c2c6376175d73ebb
Anirudh Kuppili <anirudhkuppili@gmail.com>
2025-10-16 18:00:48 -0500

    Fixed catch loops

--- src/app/components/common/react-bits/SplitText/SplitText.tsx
+++ src/app/components/common/react-bits/SplitText/SplitText.tsx
@@ -62,7 +62,9 @@ const SplitText: React.FC<SplitTextProps> = ({
       if (el._rbsplitInstance) {
         try {
           el._rbsplitInstance.revert();
-        } catch () {}
+        } catch (e) {
+          console.error(e);
+        }
         el._rbsplitInstance = undefined;
       }
 
@@ -129,7 +131,9 @@ const SplitText: React.FC<SplitTextProps> = ({
         });
         try {
           splitInstance.revert();
-        } catch {}
+        } catch (e) {
+          console.error(e);
+        }
         el._rbsplitInstance = undefined;
       };
     },
