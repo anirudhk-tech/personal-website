43e5633a1839d334d7ad439e9418d018011c8e65
Anirudh Kuppili <anirudhkuppili@gmail.com>
2025-10-20 16:58:06 -0500

    fade in fixed

--- src/app/components/landing/landing.tsx
+++ src/app/components/landing/landing.tsx
@@ -39,17 +39,19 @@ export const Landing = ({
   return (
     <Container>
       <TextContainer>
-        <SplitText
-          text="Oh hi there, I'm Ani."
-          delay={100}
-          duration={0.6}
-          ease="power3.out"
-          splitType="chars"
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          textAlign="center"
-        />
+        <FadeIn delay={0.5} duration={1}>
+          <SplitText
+            text="Oh hi there, I'm Ani."
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            textAlign="center"
+          />
+        </FadeIn>
         <FadeIn delay={1.5} duration={1}>
           <Typography variant="body1">
             Looking for something specific?
